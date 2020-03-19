1478cc7b0552c3f09b211d45d809c6314b68f30b
Samuel Joset <samuel@trusk.com>
2020-03-19 23:40:16 +0100

    test: add test when empty objects are passed

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 const groupValuesByKey = require(".");
 
 describe("chainFns tests", () => {
-  test("if groupValuesByKey() is called then it must be defined", () => {
+  test("if groupValuesByKey is called then it must be defined", () => {
     expect(typeof groupValuesByKey).toBe("function");
+  });
+
+  test("if empty objects are passed then a empty object is returned", () => {
+    expect(groupValuesByKey([{}, {}])).toEqual({});
   });
 });
