9fa8ad5e3ee593e5cde0213a2d015bd7038b7d25
Samuel Joset <samuel@trusk.com>
2020-03-19 23:32:46 +0100

    test: add define test

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 const groupValuesByKey = require(".");
 
 describe("chainFns tests", () => {
-  test.todo("if groupValuesByKey() is called then it must be defined");
+  test("if groupValuesByKey() is called then it must be defined", () => {
+    expect(typeof groupValuesByKey).toBe("function");
+  });
 });
